ced3277e8a84028e3845d9f3c4ed65759ee09a65
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-07-19 13:16:27 +0800

    node16

--- script/index.mjs
+++ script/index.mjs
@@ -1,30 +1,38 @@
 import * as fs from "fs/promises"
 
-import dataJson from "../data.json" assert {type: "json"}
 const pathStudents = await fs.readdir("./students")
 
-dataJson.classrooms.forEach(classroom => {
-  classroom.assignments.forEach(assignment => {
+const map = new Map()
+
+pathStudents.forEach((pathStudent) => {
+  const classroomAssignment = pathStudent.split("_")[0] + '_' + pathStudent.split("_")[1]
+  const pathStudentUrl = `./students/${pathStudent}`
+  map.set(classroomAssignment, [...map.get(classroomAssignment) || [], pathStudentUrl])
+})
+
+const classroomString = await fs.readFile("./data.json", "utf-8")
+
+const classroomJson = JSON.parse(classroomString)
+classroomJson.classrooms.forEach((classroom) => {
+  classroom.assignments.forEach((assignment) => {
     assignment.student_repositories = []
   })
 })
 
-
-dataJson.classrooms.forEach(classroom => {
-  classroom.assignments.forEach(assignment => {
-    pathStudents.filter(url => url.includes(classroom.title) && url.includes(assignment.title)).forEach(
-      (url, index, arr) => {
-        console.log(arr)
-        import(`../students/${url}`, { assert: { type: "json" } }).then(
-          data => {
-            assignment.student_repositories.push(data.default)
-            if (index === arr.length - 1) start()
-          }
-        )
+classroomJson.classrooms.forEach(classroom => {
+  let title = classroom.title
+  classroom.assignments.forEach((assignment, index, arr) => {
+    title = title + '_' + assignment.title
+    if (map.has(title)) {
+      map.get(title).forEach(async (pathStudentUrl, urlIndex, UrlArr) => {
+        const readJson = await fs.readFile(pathStudentUrl, "utf-8")
+        assignment.student_repositories.push(JSON.parse(readJson))
+        if (index === arr.length - 1 && urlIndex === UrlArr.length - 1) {
+          console.log(1)
+          await fs.writeFile("./data.json", JSON.stringify(classroomJson, null, 2), "utf-8")
+        }
       })
+    }
+
   })
 })
-
-function start() {
-  fs.writeFile("./data.json", JSON.stringify(dataJson, null, 2))
-}
