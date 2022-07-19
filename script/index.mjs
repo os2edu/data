602f7e80df03767d794d985311d970065177bed1
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-07-20 02:56:45 +0800

    update

--- script/index.mjs
+++ script/index.mjs
@@ -1,38 +1,19 @@
-import * as fs from "fs/promises"
+import * as fs from "fs"
 
-const pathStudents = await fs.readdir("./students")
+const pathStudents = fs.readdirSync("./students")
 
-const map = new Map()
+const classroom = [
+  {
+    "title": "forth-grade",
+    "id": "109397453-forth-grade",
+    "desc": "",
+    assignments: [
 
-pathStudents.forEach((pathStudent) => {
-  const classroomAssignment = pathStudent.split("_")[0] + '_' + pathStudent.split("_")[1]
-  const pathStudentUrl = `./students/${pathStudent}`
-  map.set(classroomAssignment, [...map.get(classroomAssignment) || [], pathStudentUrl])
+    ]
+  }
+]
+pathStudents.map(item => `./students/${item}`).forEach(item => {
+  classroom[0].assignments.push(JSON.parse(fs.readFileSync(item, "utf8")).assignment)
 })
 
-const classroomString = await fs.readFile("./data.json", "utf-8")
-
-const classroomJson = JSON.parse(classroomString)
-classroomJson.classrooms.forEach((classroom) => {
-  classroom.assignments.forEach((assignment) => {
-    assignment.student_repositories = []
-  })
-})
-
-classroomJson.classrooms.forEach(classroom => {
-  let title = classroom.title
-  classroom.assignments.forEach((assignment, index, arr) => {
-    title = title + '_' + assignment.title
-    if (map.has(title)) {
-      map.get(title).forEach(async (pathStudentUrl, urlIndex, UrlArr) => {
-        const readJson = await fs.readFile(pathStudentUrl, "utf-8")
-        assignment.student_repositories.push(JSON.parse(readJson))
-        if (index === arr.length - 1 && urlIndex === UrlArr.length - 1) {
-          console.log(1)
-          await fs.writeFile("./data.json", JSON.stringify(classroomJson, null, 2), "utf-8")
-        }
-      })
-    }
-  })
-})
-
+fs.writeFileSync("./data.json", JSON.stringify(classroom, null, 2))
