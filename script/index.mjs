a8e0f4c0258ccf92beb173daa8054718d65d478c
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-07-21 07:54:47 +0800

    merge

--- script/index.mjs
+++ script/index.mjs
@@ -12,8 +12,24 @@ const classroom = [
     ]
   }
 ]
+
+const res = JSON.parse(fs.readFileSync(`./students/${pathStudents[0]}`, "utf8"))
+
+res.forEach(item => {
+  classroom[0].assignments.push({
+    "title": item.title,
+    "branch": item.branch,
+    "student_repositories": []
+  })
+})
+
 pathStudents.map(item => `./students/${item}`).forEach(item => {
-  classroom[0].assignments.push(JSON.parse(fs.readFileSync(item, "utf8")).assignment)
+  const res = JSON.parse(fs.readFileSync(item, "utf8"))
+  res.forEach(item => {
+    classroom[0].assignments
+      .find(assignment => assignment.branch === item.branch)
+      .student_repositories.push(item)
+  })
 })
 
 fs.writeFileSync("./data.json", JSON.stringify(classroom, null, 2))
